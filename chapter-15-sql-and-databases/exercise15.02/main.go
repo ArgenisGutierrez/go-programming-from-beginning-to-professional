e2c453c5742e8d6a176ee46840aa0b9d8c1b1999
Argenis Gutierrez <argenis.v.gtz@gmail.com>
2025-02-21 22:24:14 -0600

    feat:chapter 15 exercise 02

---
+++ chapter-15-sql-and-databases/exercise15.02/main.go
@@ -0,0 +1,88 @@
+// Package main holding prime numbers in a database
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"math/big"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+func main() {
+	var (
+		number    int64
+		prop      string
+		primeSum  int64
+		newNumber int64
+	)
+	db, err := sql.Open("mysql", "arfhel:sheyla95@/go")
+	if err != nil {
+		panic(err)
+	} else {
+		fmt.Println("The connection was successfully")
+	}
+
+	allNumbers := "SELECT * FROM Numbers"
+	numbers, err := db.Prepare(allNumbers)
+	if err != nil {
+		panic(err)
+	}
+
+	primeSum = 0
+	result, err := numbers.Query()
+	fmt.Println("The list of prime numbers:")
+	for result.Next() {
+		err = result.Scan(&number, &prop)
+		if err != nil {
+			panic(err)
+		}
+		if big.NewInt(number).ProbablyPrime(0) {
+			primeSum += number
+			fmt.Println(" ", number)
+		}
+	}
+	err = numbers.Close()
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("\nThe total sum of prime numbers in this range is:", primeSum)
+
+	remove := "DELETE FROM Numbers WHERE Property=?"
+	removeResult, err := db.Exec(remove, "Even")
+	if err != nil {
+		panic(err)
+	}
+	modifiedRecords, err := removeResult.RowsAffected()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("The number of rows removed:", modifiedRecords)
+	fmt.Println("Updating numbers...")
+	update := "UPDATE Numbers SET Number=? WHERE Number=? AND Property=?"
+	allTheNumbers := "SELECT * FROM Numbers"
+	numbers, err = db.Prepare(allTheNumbers)
+	if err != nil {
+		panic(err)
+	}
+	result, err = numbers.Query()
+	for result.Next() {
+		err = result.Scan(&number, &prop)
+		if err != nil {
+			panic(err)
+		}
+		newNumber = number + primeSum
+		_, err = db.Exec(update, newNumber, number, prop)
+		if err != nil {
+			panic(err)
+		}
+	}
+	numbers.Close()
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("The execution is now complete...")
+	db.Close()
+}
