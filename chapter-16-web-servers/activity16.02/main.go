c7e4ca0b31074fa4df5845552a2a686a3deb7e74
Argenis Gutierrez <argenis.v.gtz@gmail.com>
2025-02-24 15:57:41 -0600

    feat:chapter16 end

---
+++ chapter-16-web-servers/activity16.02/main.go
@@ -0,0 +1,43 @@
+// Package main external template
+package main
+
+import (
+	"html/template"
+	"log"
+	"net/http"
+	"strings"
+)
+
+type Visitor struct {
+	Name string
+}
+type Hello struct {
+	tpl *template.Template
+}
+
+func (h Hello) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	vl := r.URL.Query()
+	cust := Visitor{}
+	name, ok := vl["name"]
+	if ok {
+		cust.Name = strings.Join(name, ",")
+	}
+	h.tpl.Execute(w, cust)
+}
+
+func NewHello(tplPath string) (*Hello, error) {
+	tmpl, err := template.ParseFiles(tplPath)
+	if err != nil {
+		return nil, err
+	}
+	return &Hello{tmpl}, nil
+}
+
+func main() {
+	hello, err := NewHello("./indext.html")
+	if err != nil {
+		log.Fatal(err)
+	}
+	http.Handle("/", hello)
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
